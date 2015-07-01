771cf85b2d7c3c8417e31d24f9e8030199f86bba
Chesheersky <d.f.goryunov@gmail.com>
2015-07-01 23:06:57 +0200

    add dummy people api

---
+++ People/routes/people.js
@@ -0,0 +1,24 @@
+﻿var express = require('express');
+var router = express.Router();
+
+router.get('/', function (req, res) {
+    res.send('This is not implemented now');
+});
+
+router.post('/', function (req, res) {
+    res.send('This is not implemented now');
+});
+
+router.get('/:id', function (req, res) {
+    res.send('This is not implemented now');
+});
+
+router.put('/:id', function (req, res) {
+    res.send('This is not implemented now');
+});
+
+router.delete('/:id', function (req, res) {
+    res.send('This is not implemented now');
+});
+
+module.exports = router;
